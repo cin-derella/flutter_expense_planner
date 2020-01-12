07169797059c1917064134f7b3564a1085a8754a
cin-derella <shirley_cinderella@outlook.com>
2020-01-12 19:58:06 +0800

    use string interpolation

--- lib/main.dart
+++ lib/main.dart
@@ -59,20 +59,36 @@ class MyHomePage extends StatelessWidget {
                       ),
                       decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         ),
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tx.amount.toString(),
+                        '¥ ${tx.amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tx.title),
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.black,
+                          ),
+                        ),
                         Text(
                           tx.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
                         )
                       ],
                     )
